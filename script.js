4e0baa2d3b202fe081874b32c55b1835be7ff462
Khushi Kashyap <khushikashyap126@gmail.com>
2022-06-21 03:31:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// ds
+// document.querySelector(".b1").addEventListener("click", function () {
+//   console.log("working");
+// });
+// document.querySelector(".b1").addEventListener("click", function () {
+//   console.log("hi baccho");
+//   // .remove();
+//   //message remove
+//   //msg.parentElemnt.removeChild(msg);
+// });
+// console.log("hi");
+// const btnCloseModal = document.querySelector(".b1");
+// // const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+// console.log(head);
+// const openModal = function () {
+//   b1.classList.remove("hidden");
+//   // overlay.classList.remove("hidden");
+// };
+// btnCloseModal.addEventListener("click", openModel);
+// const closeModal = function (e) {
+//   e.preventDefault();
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// };
+// const element = document.getElementById("demo");
+// element.addEventListener("click", myFunction);
+// function myFunction() {
+//   document.getElementById("demo1").innerHTML = "Hello World";
+// }
+// document.querySelector(".navbar-toggle").addEventListener("click", function () {
+//   console.log("hi guys");
+// });
